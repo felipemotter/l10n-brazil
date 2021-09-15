1fea8aae3aef429470a39a8ed27745ac0e657367
Felipe <felipkin@gmail.com>
2021-09-15 15:46:18 -0300

    Add module Nfe Payment

---
+++ l10n_br_nfe_payment/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2013  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Payment Informations on NFe",
+    "summary": "Add payment informations on NFe",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "version": "12.0.0.0.0",
+    "development_status": "Alpha",
+    "depends": [
+        "l10n_br_nfe",
+        "spec_driven_model",
+        "account_payment_mode",
+    ],
+    "data": [
+        # Some data
+    ],
+    "demo": [
+        # Some demo data is being loaded via post_init_hook in hook file
+    ],
+}
