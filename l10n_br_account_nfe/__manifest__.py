1a90ed85c6ca1cd53fcf0114b3cd99a064e56169
Felipe <felipkin@gmail.com>
2021-09-15 18:25:54 -0300

    Create account_nfe module

--- l10n_br_account_nfe/__manifest__.py
+++ l10n_br_account_nfe/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "NFe Payment Informations",
-    "summary": "Add payment informations on NFe",
+    "name": "Account NFe/NFC-e Integration",
+    "summary": "Integration between account and NFe",
     "category": "Localisation",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA)",
@@ -12,11 +12,10 @@
     "development_status": "Alpha",
     "depends": [
         "l10n_br_nfe",
-        "spec_driven_model",
         "account_payment_mode",
     ],
     "data": [
-        # Some data
+        "views/account_payment_mode.xml",
     ],
     "demo": [
         # Some demo data is being loaded via post_init_hook in hook file
