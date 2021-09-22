eed998831f5ebe2e57471dc536a40d7f6c92297f
Felipe <felipkin@gmail.com>
2021-09-21 21:28:41 -0300

    Set user errors and financial condition

--- l10n_br_account_nfe/models/account_invoice.py
+++ l10n_br_account_nfe/models/account_invoice.py
@@ -1,4 +1,5 @@
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 from odoo.addons.l10n_br_fiscal.constants.fiscal import (
     MODELO_FISCAL_NFCE,
@@ -26,20 +27,29 @@ class AccountInvoice(models.Model):
     def action_invoice_open(self):
         super(AccountInvoice, self).action_invoice_open()
 
-        for rec in self.filtered(filter_processador_edoc_nfe):
-            moves_terms = rec.financial_move_line_ids.filtered(
-                lambda move_line: move_line.date_maturity > move_line.date
-            )
-            indPag = "1" if len(moves_terms) > 0 else "0"
-            rec.nfe40_detPag = [
-                (5, 0, 0),
-                (
-                    0,
-                    0,
-                    rec.fiscal_document_id._prepare_amount_financial(
-                        indPag,
-                        rec.payment_mode_id.fiscal_payment_mode,
-                        rec.amount_financial_total,
-                    ),
+        for inv in self.filtered(filter_processador_edoc_nfe):
+            if inv.amount_financial_total > 0:
+                self.generate_payment_info(inv)
+
+    def generate_payment_info(self, inv):
+
+        if not inv.payment_mode_id.fiscal_payment_mode:
+            raise UserError(_("Document without Fiscal Payment Mode!"))
+
+        moves_terms = inv.financial_move_line_ids.filtered(
+            lambda move_line: move_line.date_maturity > move_line.date
+        )
+        indPag = "1" if len(moves_terms) > 0 else "0"
+
+        inv.nfe40_detPag = [
+            (5, 0, 0),
+            (
+                0,
+                0,
+                inv.fiscal_document_id._prepare_amount_financial(
+                    indPag,
+                    inv.payment_mode_id.fiscal_payment_mode,
+                    inv.amount_financial_total,
                 ),
-            ]
+            ),
+        ]
