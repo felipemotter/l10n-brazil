cb2bab4b9110456e39b300cb597487682f6b296c
Felipe <felipkin@gmail.com>
2021-09-23 17:05:55 -0300

    Testes in progress

--- l10n_br_account_nfe/tests/test_generate_payment_info.py
+++ l10n_br_account_nfe/tests/test_generate_payment_info.py
@@ -6,7 +6,8 @@ class TestGeneratePaymentInfo(TransactionCase):
         super(TestGeneratePaymentInfo, self).setUp()
 
         self.company = self.env.ref("l10n_br_base.empresa_lucro_presumido")
-        payment_mode = self.env["account.payment.mode"].create(
+
+        self.payment_mode = self.env["account.payment.mode"].create(
             {
                 "name": "Money",
                 "company_id": self.company.id,
@@ -17,68 +18,93 @@ class TestGeneratePaymentInfo(TransactionCase):
                 "bank_account_link": "variable",
             }
         )
-        payment_mode.fiscal_payment_mode = "18"
 
-        invoice_account_id = (
-            self.env["account.account"]
-            .create(
-                {
-                    "company_id": self.company.id,
-                    "user_type_id": self.env.ref(
-                        "account.data_account_type_receivable"
-                    ).id,
-                    "code": "RECTEST",
-                    "name": "Test receivable account",
-                    "reconcile": True,
-                }
-            )
-            .id
+        self.invoice_account_id = self.env["account.account"].create(
+            {
+                "company_id": self.company.id,
+                "user_type_id": self.env.ref("account.data_account_type_receivable").id,
+                "code": "RECTEST",
+                "name": "Test receivable account",
+                "reconcile": True,
+            }
+        )
+
+        self.invoice_journal = self.env["account.journal"].create(
+            {
+                "company_id": self.company.id,
+                "name": "Invoice Journal - (test)",
+                "code": "INVTEST",
+                "type": "purchase",
+                "update_posted": True,
+            }
+        )
+
+        self.payment_term = self.env["account.payment.term"].create(
+            {
+                "name": "30 Days",
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "balance",
+                            "days": 30,
+                            "option": "day_after_invoice_date",
+                        },
+                    )
+                ],
+            }
         )
 
         self.invoice = self.env["account.invoice"].create(
             {
                 "company_id": self.company.id,
                 "partner_id": self.env.ref("l10n_br_base.res_partner_cliente1_sp").id,
-                "payment_mode_id": payment_mode.id,
+                "payment_mode_id": self.payment_mode.id,
                 "document_type_id": self.env.ref("l10n_br_fiscal.document_55").id,
                 "fiscal_operation_id": self.env.ref("l10n_br_fiscal.fo_venda").id,
-                "issuer": "company",
                 "document_serie_id": self.env.ref(
                     "l10n_br_fiscal.empresa_lc_document_55_serie_1"
                 ).id,
-                "account_id": invoice_account_id,
+                "journal_id": self.invoice_journal.id,
+                "payment_term_id": self.payment_term.id,
             }
         )
 
-        invoice_line_account_id = (
-            self.env["account.account"]
-            .create(
-                {
-                    "company_id": self.company.id,
-                    "user_type_id": self.env.ref(
-                        "account.data_account_type_expenses"
-                    ).id,
-                    "code": "EXPTEST",
-                    "name": "Test expense account",
-                }
-            )
-            .id
+        self.invoice_line_account_id = self.env["account.account"].create(
+            {
+                "company_id": self.company.id,
+                "user_type_id": self.env.ref("account.data_account_type_expenses").id,
+                "code": "EXPTEST",
+                "name": "Test expense account",
+            }
         )
 
-        self.env["account.invoice.line"].create(
+        self.line = self.env["account.invoice.line"].create(
             {
-                "product_id": self.env.ref("product.product_product_4").id,
+                "product_id": self.env.ref("product.product_product_3").id,
                 "quantity": 1,
-                "price_unit": 100,
                 "invoice_id": self.invoice.id,
                 "name": "something",
                 "fiscal_operation_id": self.env.ref("l10n_br_fiscal.fo_venda").id,
                 "fiscal_operation_line_id": self.env.ref(
                     "l10n_br_fiscal.fo_venda_venda"
                 ).id,
-                "account_id": invoice_line_account_id,
+                "account_id": self.invoice_line_account_id.id,
             }
         )
 
+        self.line._onchange_product_id_fiscal()
+        self.line._onchange_commercial_quantity()
+        self.line._onchange_ncm_id()
+        self.line._onchange_fiscal_operation_id()
+        self.line._onchange_fiscal_operation_line_id()
+        self.line._onchange_fiscal_taxes()
+
     def test_generate_payment_info(self):
         self.invoice.action_invoice_open()
+
+        for detPag in self.invoice.nfe40_detPag:
+            self.assertEqual(detPag.nfe40_indPag, "1", "Error in nfe40_indPag field.")
+            self.assertEqual(detPag.nfe40_tPag, "18", "Error in nfe40_tPag field.")
+            self.assertEqual(detPag.nfe40_vPag, 450, "Error in nfe40_vPag field.")
