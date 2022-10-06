60b32ae2c5b26f6145ade326c6ed09af6c6ae2c5
Neto <netosjb@yahoo.com.br>
2022-10-05 21:54:02 -0300

    [REF] payment_order: service_type refactor

--- l10n_br_account_payment_order/models/account_move_line.py
+++ l10n_br_account_payment_order/models/account_move_line.py
@@ -6,7 +6,12 @@
 
 from odoo import api, fields, models
 
-from ..constants import BR_CODES_PAYMENT_ORDER, ESTADOS_CNAB, SITUACAO_PAGAMENTO
+from ..constants import (
+    BR_CODES_PAYMENT_ORDER,
+    ESTADOS_CNAB,
+    PAGAMENTO_FORNECEDOR,
+    SITUACAO_PAGAMENTO,
+)
 
 
 class AccountMoveLine(models.Model):
@@ -124,6 +129,10 @@ class AccountMoveLine(models.Model):
             else:
                 record.journal_entry_ref = "*" + str(record.move_id.id)
 
+    def _get_default_service_type(self):
+        if self.move_id.move_type == "in_invoice":
+            return PAGAMENTO_FORNECEDOR
+
     def _prepare_payment_line_vals(self, payment_order):
         vals = super()._prepare_payment_line_vals(payment_order)
         # Preenchendo apenas nos casos CNAB
@@ -144,6 +153,7 @@ class AccountMoveLine(models.Model):
                     "ml_maturity_date": self.date_maturity,
                     "move_id": self.move_id.id,
                     "payment_way_id": self.payment_way_id.id,
+                    "service_type": self._get_default_service_type(),
                 }
             )
 
