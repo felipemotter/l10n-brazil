eab1ac712b8c3ea55b8460b4d9a4ae18a246b796
Antônio Neto <neto@engenere.one>
2023-02-10 22:57:52 -0300

    [REF] l10n_br_account: remove fiscal dummy

--- l10n_br_account/models/account_invoice_line.py
+++ l10n_br_account/models/account_invoice_line.py
@@ -149,23 +149,9 @@ class AccountMoveLine(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        dummy_doc = self.env.company.fiscal_dummy_id
-        dummy_line = fields.first(dummy_doc.fiscal_line_ids)
         for values in vals_list:
             move_id = self.env["account.move"].browse(values["move_id"])
             fiscal_doc_id = move_id.fiscal_document_id.id
-
-            if fiscal_doc_id == dummy_doc.id or values.get("exclude_from_invoice_tab"):
-                if len(dummy_line) < 1:
-                    raise UserError(
-                        _(
-                            "Document line dummy not found. Please contact "
-                            "your system administrator."
-                        )
-                    )
-                values["fiscal_document_line_id"] = dummy_line.id
-                continue  # dummy doc line, we can skip all l10n-brazil logic
-
             values.update(
                 self._update_fiscal_quantity(
                     values.get("product_id"),
@@ -217,11 +203,8 @@ class AccountMoveLine(models.Model):
         return super().create(vals_list)
 
     def write(self, values):
-        dummy_doc = self.env.company.fiscal_dummy_id
-        dummy_line = fields.first(dummy_doc.fiscal_line_ids)
-        non_dummy = self.filtered(lambda l: l.fiscal_document_line_id != dummy_line)
         self._inject_shadowed_fields([values])
-        if values.get("move_id") and len(non_dummy) == len(self):
+        if values.get("move_id"):
             # we can write the document_id in all lines
             values["document_id"] = (
                 self.env["account.move"].browse(values["move_id"]).fiscal_document_id.id
@@ -233,7 +216,7 @@ class AccountMoveLine(models.Model):
             doc_id = (
                 self.env["account.move"].browse(values["move_id"]).fiscal_document_id.id
             )
-            super(AccountMoveLine, non_dummy).write({"document_id": doc_id})
+            super(AccountMoveLine).write({"document_id": doc_id})
         else:
             result = super().write(values)
 
@@ -268,18 +251,16 @@ class AccountMoveLine(models.Model):
                     cfop_id=line.cfop_id,
                 )
                 result |= super(AccountMoveLine, line).write(to_write)
-
+            shadowed_fiscal_vals = line._prepare_shadowed_fields_dict()
+            line.fiscal_document_line_id.write(shadowed_fiscal_vals)
         return result
 
     def unlink(self):
-        dummy_doc = self.env.company.fiscal_dummy_id
-        dummy_line = fields.first(dummy_doc.fiscal_line_ids)
         unlink_fiscal_lines = self.env["l10n_br_fiscal.document.line"]
         for inv_line in self:
             if not inv_line.exists():
                 continue
-            if inv_line.fiscal_document_line_id.id != dummy_line.id:
-                unlink_fiscal_lines |= inv_line.fiscal_document_line_id
+            unlink_fiscal_lines |= inv_line.fiscal_document_line_id
         result = super().unlink()
         unlink_fiscal_lines.unlink()
         self.clear_caches()
