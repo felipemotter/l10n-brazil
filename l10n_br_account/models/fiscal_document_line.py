7ebdbeba5addcff28d0004e2813a67a472b73e61
Felipe Motter <felipe@engenere.one>
2023-04-11 11:04:12 -0300

    [FIX] l10n_br_account: remove modified triggers

--- l10n_br_account/models/fiscal_document_line.py
+++ l10n_br_account/models/fiscal_document_line.py
@@ -23,26 +23,3 @@ class FiscalDocumentLine(models.Model):
     fiscal_quantity = fields.Float(related="quantity", readonly=False)
     fiscal_price_unit = fields.Float(related="price_unit", readonly=False)
 
-    def modified(self, fnames, create=False, before=False):
-        """
-        Modifying a dummy fiscal document (no document_type_id) line should not recompute
-        any account.move.line related to the same dummy fiscal document line.
-        """
-        filtered = self.filtered(
-            lambda rec: isinstance(rec.id, models.NewId)
-            or not rec.document_id  # document_id might exist and be computed later
-            or rec.document_id.document_type_id
-        )
-        return super(FiscalDocumentLine, filtered).modified(fnames, create, before)
-
-    def _modified_triggers(self, tree, create=False):
-        """
-        Modifying a dummy fiscal document (no document_type_id) line should not recompute
-        any account.move.line related to the same dummy fiscal document line.
-        """
-        filtered = self.filtered(
-            lambda rec: isinstance(rec.id, models.NewId)
-            or not rec.document_id  # document_id might exist and be computed later
-            or rec.document_id.document_type_id
-        )
-        return super(FiscalDocumentLine, filtered)._modified_triggers(tree, create)
