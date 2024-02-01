3a1c4d859b8f85373b99bb712825905fc7ace0d7
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-02-01 16:17:19 -0300

    [IMP] l10n_br_ie_search: add assistant to IE

--- l10n_br_ie_search/wizard/extend_partner_cnpj_search_wizard.py
+++ l10n_br_ie_search/wizard/extend_partner_cnpj_search_wizard.py
@@ -1,10 +1,13 @@
 # Copyright 2023 KMEE - Breno Oliveira Dias
+# Copyright (C) 2024-Today - Engenere (<https://engenere.one>).
+# @author Cristiano Mafra Junior
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import requests
 from erpbrasil.assinatura import certificado as cert
 from erpbrasil.edoc.nfe import NFe as edoc_nfe
 from erpbrasil.transmissao import TransmissaoSOAP
-from requests import Session, get
+from requests import Session
 
 from odoo import _, api, models
 from odoo.exceptions import UserError
@@ -12,44 +15,26 @@ from odoo.exceptions import UserError
 SINTEGRA_URL = "https://www.sintegraws.com.br/api/v1/execute-api.php"
 
 
-class PartyMixin(models.AbstractModel):
-    _inherit = "l10n_br_base.party.mixin"
+class ExtendPartnerCnpjSearchWizard(models.TransientModel):
+    _inherit = "partner.search.wizard"
 
-    def search_cnpj(self):
-        """Search state subscription"""
-
-        super().search_cnpj()
-        self.ie_search()
-
-    @api.model
-    def ie_search(self, mockresponse=False):
+    def _get_partner_ie(self, state_code, cnpj):
         webservice = self.env["l10n_br_cnpj_search.webservice.abstract"]
         if self._provider() == "sefaz":
             processo = self._processador()
-            response = (
-                webservice.sefaz_search(self.state_id.code, self.cnpj_cpf, processo)
-                if not mockresponse
-                else mockresponse
-            )
+            response = webservice.sefaz_search(state_code, cnpj, processo)
             data = webservice.sefaz_validate(response)
             values = webservice._sefaz_import_data(data)
-            self.write(values)
+            return values
         elif self._provider() == "sintegraws":
-            response = (
-                get(
-                    SINTEGRA_URL,
-                    data="",
-                    params=webservice._get_query(
-                        self.cnpj_cpf, webservice._get_token()
-                    ),
-                )
-                if not mockresponse
-                else mockresponse
+            response = requests.get(
+                SINTEGRA_URL,
+                data="",
+                params=webservice._get_query(cnpj, webservice._get_token()),
             )
-
             data = webservice.sintegra_validate(response)
             values = webservice._sintegra_import_data(data)
-            self.write(values)
+            return values
 
     @api.model
     def _provider(self):
@@ -58,6 +43,15 @@ class PartyMixin(models.AbstractModel):
             .sudo()
             .get_param("l10n_br_ie_search.ie_search")
         )
+
+    @api.model
+    def _get_partner_values(self, cnpj_cpf):
+        values = super()._get_partner_values(cnpj_cpf)
+        state_id = self.env["res.country.state"].browse(values["state_id"])
+        ie_values = self._get_partner_ie(state_code=state_id.code, cnpj=cnpj_cpf)
+        if ie_values:
+            values.update(ie_values)
+        return values
 
     @api.model
     def _processador(self):
